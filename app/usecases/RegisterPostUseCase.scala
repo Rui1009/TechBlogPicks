05b57e23f014d662ecd75e8ff476fc9d7d52ddd3
Daiki <daikidevelopment@gmail.com>
2021-03-09 18:44:45 +0900

    wip

--- app/usecases/RegisterPostUseCase.scala
+++ app/usecases/RegisterPostUseCase.scala
@@ -1,14 +1,15 @@
 package usecases
 
 import com.google.inject.Inject
-import domains.bot.PostRepository
+import domains.post.PostRepository
 import domains.post.Post
 import domains.post.Post._
 import usecases.RegisterPostUseCase.Params
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext
 
 trait RegisterPostUseCase {
-  def exec(params: Params): Future[Unit]
+  def exec(params: Params): Future[Either[UseCaseError, Unit]]
 }
 
 object RegisterPostUseCase {
@@ -21,9 +22,15 @@ object RegisterPostUseCase {
 
 final class RegisterPostUseCaseImpl @Inject()(
     postRepository: PostRepository
-) extends RegisterPostUseCase {
-  override def exec(params: Params): Future[Unit] = {
+)(implicit val ec: ExecutionContext)
+    extends RegisterPostUseCase {
+  override def exec(params: Params): Future[Either[SystemError, Unit]] = {
     val post = Post(None, params.url, params.title, params.postedAt)
-    ???
+    postRepository.Add(post).map {
+      case Right(v) => Right(v)
+      case Left(e) =>
+        Left(
+          SystemError("error while postRepository.add" + "\n" + e.errorMessage))
+    }
   }
 }
