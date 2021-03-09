eaac5b95bf5574ba36ffe5a0f99bbdf581093e0d
Daiki <daikidevelopment@gmail.com>
2021-03-09 19:42:19 +0900

    fix ops

--- app/usecases/ops/FutureOps.scala
+++ app/usecases/ops/FutureOps.scala
@@ -1,7 +1,7 @@
 package usecases.ops
 
 import infra.InfraError
-import usecases.SystemError
+import usecases.{SystemError, UseCaseError}
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
@@ -11,13 +11,13 @@ trait FutureOps {
       futureEither: Future[Either[E, R]]
   )(implicit val ec: ExecutionContext) {
     def ifLeftThenToUseCaseError(
-        message: String): Future[Either[SystemError, R]] =
+        message: String): Future[Either[UseCaseError, R]] =
       futureEither.transformWith {
         case Success(Right(v)) => Future.successful(Right(v))
         case Success(Left(e)) =>
-          Future.successful(Left(SystemError(message + "\n" + e.errorMessage)))
-        case Failure(exception) =>
-          Future.failed(SystemError(message + "\n" + exception.getMessage))
+          Future.successful(Left(SystemError(message + "\n" + e.getMessage)))
+        case Failure(e) =>
+          Future.failed(SystemError(message + "\n" + e.getMessage))
       }
   }
 }
