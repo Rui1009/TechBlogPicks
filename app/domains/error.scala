628565a57b00982ba63016686fbd9c170ab57421
Daiki <daikidevelopment@gmail.com>
2021-03-09 16:52:34 +0900

    add regex error

--- app/domains/error.scala
+++ app/domains/error.scala
@@ -12,3 +12,7 @@ final case class EmptyStringError(className: String) extends DomainError {
 final case class NegativeNumberError(className: String) extends DomainError {
   override lazy val content: String = s"$className is negative number"
 }
+
+final case class RegexError(className: String) extends DomainError {
+  override lazy val content: String = s"$className don't match pattern"
+}
