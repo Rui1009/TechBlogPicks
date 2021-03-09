0004a3c7d763307a17a1ec5f28358bf9b41073ab
Daiki <daikidevelopment@gmail.com>
2021-03-09 18:28:57 +0900

    wip

--- app/usecases/RegisterPostUseCase.scala
+++ app/usecases/RegisterPostUseCase.scala
@@ -2,20 +2,28 @@ package usecases
 
 import com.google.inject.Inject
 import domains.bot.PostRepository
+import domains.post.Post
 import domains.post.Post._
-
+import usecases.RegisterPostUseCase.Params
 import scala.concurrent.Future
 
 trait RegisterPostUseCase {
-  def exec(): Future[Unit]
+  def exec(params: Params): Future[Unit]
 }
 
 object RegisterPostUseCase {
-  final case class Params(url: PostUrl, title: PostTitle, postedAt: PostedAt)
+  final case class Params(
+      url: Option[PostUrl],
+      title: PostTitle,
+      postedAt: PostPostedAt
+  )
 }
 
 final class RegisterPostUseCaseImpl @Inject()(
     postRepository: PostRepository
 ) extends RegisterPostUseCase {
-  override def exec(): Future[Unit] = ???
+  override def exec(params: Params): Future[Unit] = {
+    val post = Post(None, params.url, params.title, params.postedAt)
+    ???
+  }
 }
