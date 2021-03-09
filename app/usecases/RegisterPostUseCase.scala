dabdff7ff7a0e04f48a4770798d31dc6a0681ec2
Daiki <daikidevelopment@gmail.com>
2021-03-09 19:25:06 +0900

    wip

--- app/usecases/RegisterPostUseCase.scala
+++ app/usecases/RegisterPostUseCase.scala
@@ -5,8 +5,10 @@ import domains.post.PostRepository
 import domains.post.Post
 import domains.post.Post._
 import usecases.RegisterPostUseCase.Params
+
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
+import scala.util.{Failure, Success}
 
 trait RegisterPostUseCase {
   def exec(params: Params): Future[Either[UseCaseError, Unit]]
@@ -26,11 +28,10 @@ final class RegisterPostUseCaseImpl @Inject()(
     extends RegisterPostUseCase {
   override def exec(params: Params): Future[Either[SystemError, Unit]] = {
     val post = Post(None, params.url, params.title, params.postedAt)
-    postRepository.Add(post).map {
-      case Right(v) => Right(v)
-      case Left(e) =>
-        Left(
-          SystemError("error while postRepository.add" + "\n" + e.errorMessage))
-    }
+    postRepository
+      .add(post)
+      .ifLeftThenToUseCaseError(
+        "error while postRepository.add in register post use case"
+      )
   }
 }
