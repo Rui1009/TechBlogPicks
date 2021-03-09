31a6c07c64b39df567a723e1501dad407f00fe20
Daiki <daikidevelopment@gmail.com>
2021-03-09 17:14:52 +0900

    add number gen

---
+++ test/helpers/gens/number.scala
@@ -0,0 +1,13 @@
+package helpers.gens
+
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.numeric.Positive
+import eu.timepit.refined.refineV
+import org.scalacheck.Gen
+
+object number extends NumberGen
+
+trait NumberGen {
+  val longRefinedPositiveGen: Gen[Refined[Long, Positive]] =
+    Gen.posNum[Long].map(n => refineV[Positive](n).right.get)
+}
