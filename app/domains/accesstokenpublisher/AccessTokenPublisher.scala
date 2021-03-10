92f063bb3b2f82f65981a15660ae8069360679dc
Rui <3116049c@gmail.com>
2021-03-10 10:32:31 +0900

    accessTokenPublisherにtemporaryOauthCodeを追加

--- app/domains/accesstokenpublisher/AccessTokenPublisher.scala
+++ app/domains/accesstokenpublisher/AccessTokenPublisher.scala
@@ -1,13 +1,16 @@
 package domains.accesstokenpublisher
 
 import domains.EmptyStringError
-import domains.accesstokenpublisher.AccessTokenPublisher.AccessTokenPublisherToken
+import domains.accesstokenpublisher.AccessTokenPublisher.{AccessTokenPublisherTemporaryOauthCode, AccessTokenPublisherToken}
 import eu.timepit.refined.api.Refined
 import eu.timepit.refined.collection.NonEmpty
 import eu.timepit.refined.refineV
 import io.estatico.newtype.macros.newtype
 
-final case class AccessTokenPublisher(token: AccessTokenPublisherToken) {
+final case class AccessTokenPublisher(
+   token: AccessTokenPublisherToken,
+   temporaryOauthCode: AccessTokenPublisherTemporaryOauthCode
+) {
   def publishToken: AccessTokenPublisherToken =
     AccessTokenPublisherToken(token.value)
 }
@@ -20,6 +23,16 @@ object AccessTokenPublisher {
       refineV[NonEmpty](value) match {
         case Right(v) => Right(AccessTokenPublisherToken(v))
         case Left(_)  => Left(EmptyStringError("Token"))
+      }
+  }
+
+  @newtype case class AccessTokenPublisherTemporaryOauthCode(value: String Refined NonEmpty)
+  object AccessTokenPublisherTemporaryOauthCode {
+    def create(
+                value: String): Either[EmptyStringError, AccessTokenPublisherTemporaryOauthCode] =
+      refineV[NonEmpty](value) match {
+        case Right(v) => Right(AccessTokenPublisherTemporaryOauthCode(v))
+        case Left(_)  => Left(EmptyStringError("AccessTokenPublisherTemporaryOauthCode"))
       }
   }
 }
