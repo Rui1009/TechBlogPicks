35d34e646406cc59e7d5da5e2a7394b66dba4c36
Rui <3116049c@gmail.com>
2021-03-10 10:32:52 +0900

    accessTokenPublisherのrepo実装

--- app/domains/accesstokenpublisher/AccessTokenPublisherRepository.scala
+++ app/domains/accesstokenpublisher/AccessTokenPublisherRepository.scala
@@ -1,7 +1,10 @@
 package domains.accesstokenpublisher
 
+import domains.accesstokenpublisher.AccessTokenPublisher.AccessTokenPublisherTemporaryOauthCode
+import infra.InfraError
+
 import scala.concurrent.Future
 
 trait AccessTokenPublisherRepository {
-  def publish(code: String): Future[AccessTokenPublisher]
+  def publish(code: AccessTokenPublisherTemporaryOauthCode): Future[Either[InfraError, AccessTokenPublisher]]
 }
