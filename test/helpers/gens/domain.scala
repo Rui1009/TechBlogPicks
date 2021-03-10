fb7e2843f9fdff51397a4cfbfb7537bfd59eee26
Daiki <daikidevelopment@gmail.com>
2021-03-10 13:10:01 +0900

    add post gen

--- test/helpers/gens/domain.scala
+++ test/helpers/gens/domain.scala
@@ -2,12 +2,14 @@ package helpers.gens
 
 import domains.accesstokenpublisher.AccessTokenPublisher
 import domains.accesstokenpublisher.AccessTokenPublisher.AccessTokenPublisherToken
+import domains.post.Post._
 import org.scalacheck.Gen
 import helpers.gens.string._
+import helpers.gens.number._
 
 object domain extends DomainGen
 
-trait DomainGen extends AccessTokenPublisherGen
+trait DomainGen extends AccessTokenPublisherGen with PostGen
 
 trait AccessTokenPublisherGen {
   val accessTokenGen: Gen[AccessTokenPublisherToken] =
@@ -15,4 +17,13 @@ trait AccessTokenPublisherGen {
 
   val accessTokenPublisherGen: Gen[AccessTokenPublisher] =
     accessTokenGen.map(AccessTokenPublisher(_))
+}
+
+trait PostGen {
+  val postUrlGen: Gen[PostUrl] = stringRefinedUrlGen.map(PostUrl(_))
+
+  val postTitleGen: Gen[PostTitle] = stringRefinedNonEmptyGen.map(PostTitle(_))
+
+  val postPostedAtGen: Gen[PostPostedAt] =
+    longRefinedPositiveGen.map(PostPostedAt(_))
 }
