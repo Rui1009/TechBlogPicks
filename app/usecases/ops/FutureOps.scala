7411810109b578c69d4a47d1bd8edc89d8a25665
Daiki <daikidevelopment@gmail.com>
2021-03-10 13:05:19 +0900

    fix class name

--- app/usecases/ops/FutureOps.scala
+++ app/usecases/ops/FutureOps.scala
@@ -1,18 +1,18 @@
 package usecases.ops
 
-import usecases.SystemError
+import usecases.{SystemError, UseCaseError}
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
 trait FutureOps {
-  implicit class FutureOps[T](future: Future[T])(
+  implicit class FutureTOps[T](future: Future[T])(
       implicit ec: ExecutionContext) {
     def ifFailThenToUseCaseError(message: String): Future[T] =
       future.transformWith {
         case Success(v) => Future.successful(v)
         case Failure(exception) =>
-          Future.failed(SystemError(message + "\n" + exception.getMessage))
+          Future.failed(SystemError(message + exception.getMessage))
       }
   }
 }
