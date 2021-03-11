3da5c90412917020889b8ceddfd50853fdb32cc9
Daiki <daikidevelopment@gmail.com>
2021-03-11 16:09:07 +0900

    fix extends trait

--- test/helpers/traits/RepositorySpec.scala
+++ test/helpers/traits/RepositorySpec.scala
@@ -2,7 +2,6 @@ package helpers.traits
 
 import scala.reflect.ClassTag
 
-abstract class RepositorySpec[T: ClassTag]
-    extends UnitSpec with HasApplication {
+abstract class RepositorySpec[T: ClassTag] extends UnitSpec with HasDB {
   final val repository: T = app.injector.instanceOf[T]
 }
