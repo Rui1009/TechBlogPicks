15c4d406a6e22e4becf9632e2b9c5e3af71017bf
Daiki <daikidevelopment@gmail.com>
2021-03-11 15:15:53 +0900

    add domain converter

---
+++ app/infra/syntax/domain.scala
@@ -0,0 +1,21 @@
+package infra.syntax
+
+import domains.post.Post
+import infra.dto.Tables._
+
+object domain extends DomainSyntax
+
+trait DomainSyntax {
+  implicit final def infraSyntaxPost(model: Post): PostOps = new PostOps(model)
+}
+
+final private[syntax] class PostOps(private val model: Post) {
+  def toRow(unixSec: Long): PostsRow = PostsRow(
+    model.id.map(_.value.value).getOrElse(0),
+    model.url.map(_.value.value),
+    model.title.value.value,
+    model.author.value.value,
+    model.postedAt.value.value,
+    unixSec
+  )
+}
