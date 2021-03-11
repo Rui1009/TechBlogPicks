406562089966be080293375dfc7f73cafc608075
Daiki <daikidevelopment@gmail.com>
2021-03-11 19:57:48 +0900

    add repo impl

--- app/infra/repositoryimpl/PostRepositoryImpl.scala
+++ app/infra/repositoryimpl/PostRepositoryImpl.scala
@@ -1,14 +1,13 @@
 package infra.repositoryimpl
 
 import com.google.inject.Inject
-import domains.bot.Bot
 import domains.bot.Bot.BotId
 import domains.post.{Post, PostRepository}
 import infra.dto.Tables._
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.jdbc.PostgresProfile
-import infra.syntax.domain._
 import slick.jdbc.PostgresProfile.API
+import infra.syntax.all._
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -18,13 +17,16 @@ class PostRepositoryImpl @Inject() (
     extends HasDatabaseConfigProvider[PostgresProfile] with PostRepository
     with API {
   override def add(model: Post, botIds: Seq[BotId]): Future[Unit] = {
-    val postsInsertAction     = Posts.returning(Posts.map(_.id)) += model.toRow(1)
-    val botsPostsInsertAction =
-      for {
-        postId <- postsInsertAction
-      } yield for {
-        botId <- botIds
-      } yield BotsPosts += BotsPostsRow(0, botId.value.value, postId)
-    db.run(DBIO.seq(postsInsertAction, botsPostsInsertAction).transactionally)
-  }
+    val nowUnix      = System.currentTimeMillis / 1000
+    val newPost      = model.toRow(nowUnix)
+    val postsInsertQ =
+      Posts.returning(Posts.map(_.id)).into((_, id) => id) += newPost
+    val query        = for {
+      postId <- postsInsertQ
+      news    = botIds.map(id => BotsPostsRow(0, id.value.value, postId))
+      _      <- BotsPosts ++= news
+    } yield ()
+
+    db.run(query.transactionally)
+  }.ifFailedThenToInfraError("error while PostRepository.add")
 }
