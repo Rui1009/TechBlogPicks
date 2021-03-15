6a9eadffd4c5cc94cb061996fc194c3de43cd64e
Daiki <daikidevelopment@gmail.com>
2021-03-15 16:58:14 +0900

    add either syntax

---
+++ test/syntax/either.scala
@@ -0,0 +1,18 @@
+package syntax
+
+object either extends EitherSyntax
+
+trait EitherSyntax {
+
+  implicit final def testSyntaxEither[R, L](e: Either[L, R]): EitherOps[R, L] =
+    new EitherOps(e)
+
+}
+
+final class EitherOps[R, L](private val e: Either[L, R]) extends AnyVal {
+
+  def unsafeGet: R = e.getOrElse(
+    throw new Exception(s"can't get right value from ${e.toString}")
+  )
+
+}
