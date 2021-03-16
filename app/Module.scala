f918d2ffe92c89c85bb8f131c07302d0cf67b4a3
Daiki <daikidevelopment@gmail.com>
2021-03-16 14:11:01 +0900

    add module

--- app/Module.scala
+++ app/Module.scala
@@ -1,11 +1,16 @@
 import com.google.inject.AbstractModule
 import domains.post.PostRepository
+import infra.queryprocessorimpl.PublishPostsQueryProcessorImpl
 import infra.repositoryimpl.PostRepositoryImpl
+import query.publishposts.PublishPostsQueryProcessor
 import usecases.{RegisterPostUseCase, RegisterPostUseCaseImpl}
 
 class Module extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[PostRepository]).to(classOf[PostRepositoryImpl])
     bind(classOf[RegisterPostUseCase]).to(classOf[RegisterPostUseCaseImpl])
+
+    bind(classOf[PublishPostsQueryProcessor])
+      .to(classOf[PublishPostsQueryProcessorImpl])
   }
 }
