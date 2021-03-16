611071eb91a411a655d1a6df2ef681c5ea4e7a2e
Daiki <daikidevelopment@gmail.com>
2021-03-16 17:58:12 +0900

    fix to lazy

--- test/helpers/traits/QueryProcessorSpec.scala
+++ test/helpers/traits/QueryProcessorSpec.scala
@@ -3,5 +3,5 @@ package helpers.traits
 import scala.reflect.ClassTag
 
 abstract class QueryProcessorSpec[T: ClassTag] extends UnitSpec with HasDB {
-  final val queryProcessor: T = app.injector.instanceOf[T]
+  final lazy val queryProcessor: T = app.injector.instanceOf[T]
 }
