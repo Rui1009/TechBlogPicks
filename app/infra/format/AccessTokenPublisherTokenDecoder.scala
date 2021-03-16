b3a218379fa51bd1acb80b9bae1a57a1031dabd6
Rui <3116049c@gmail.com>
2021-03-16 12:03:44 +0900

    decoderの実装

--- app/infra/format/AccessTokenPublisherTokenDecoder.scala
+++ app/infra/format/AccessTokenPublisherTokenDecoder.scala
@@ -1,13 +1,16 @@
 package infra.format
 
-import domains.accesstokenpublisher.AccessTokenPublisher
+import domains.accesstokenpublisher.AccessTokenPublisher.AccessTokenPublisherToken
+import eu.timepit.refined.api.Refined
 import io.circe.Decoder
+import eu.timepit.refined.auto._
 
-trait AccessTokenPublisherDecoder {
-  implicit val decodeAccessTokenPublisher: Decoder[AccessTokenPublisher] = Decoder.instance { cursor =>
+trait AccessTokenPublisherTokenDecoder {
+  implicit val decodeAccessTokenPublisherToken
+    : Decoder[AccessTokenPublisherToken] = Decoder.instance { cursor =>
     for {
       accessTokenPublisherToken <- cursor.downField("access_token").as[String]
-    } yield AccessTokenPublisher()
+    } yield AccessTokenPublisherToken(Refined.unsafeApply(accessTokenPublisherToken))
 
   }
 }
