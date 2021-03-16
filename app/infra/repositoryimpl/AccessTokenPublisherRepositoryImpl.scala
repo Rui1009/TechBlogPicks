f1900d8d8860049985881b03a684c31fcdde0f80
Rui <3116049c@gmail.com>
2021-03-16 12:05:43 +0900

    access token publisher のrepoImpl実装

--- app/infra/repositoryimpl/AccessTokenPublisherRepositoryImpl.scala
+++ app/infra/repositoryimpl/AccessTokenPublisherRepositoryImpl.scala
@@ -3,33 +3,43 @@ package infra.repositoryimpl
 import com.google.inject.Inject
 import domains.{DomainError, EmptyStringError}
 import domains.accesstokenpublisher.AccessTokenPublisher.AccessTokenPublisherToken
-import domains.accesstokenpublisher.{AccessTokenPublisher, AccessTokenPublisherRepository}
+import domains.accesstokenpublisher.{
+  AccessTokenPublisher,
+  AccessTokenPublisherRepository
+}
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.libs.json.Json
 import play.api.libs.ws._
 import slick.jdbc.PostgresProfile
 import slick.jdbc.PostgresProfile.API
 import eu.timepit.refined.auto._
+import infra.format.AccessTokenPublisherTokenDecoder
+import io.circe.parser._
+import infra.syntax.either._
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class AccessTokenPublisherRepositoryImpl @Inject() (
   protected val dbConfigProvider: DatabaseConfigProvider,
   protected val ws: WSClient
-) (implicit val ec: ExecutionContext) extends HasDatabaseConfigProvider[PostgresProfile] with AccessTokenPublisherRepository
-  with API {
-  override def find(code: AccessTokenPublisher.AccessTokenPublisherTemporaryOauthCode): Future[AccessTokenPublisher] = {
-    val oauthURL = "https://slack.com/api/oauth.v2.access"
-    val postedParam = Json.obj(
-      "code" -> code.value.value
-    )
+)(implicit val ec: ExecutionContext)
+    extends HasDatabaseConfigProvider[PostgresProfile]
+    with AccessTokenPublisherRepository with API
+    with AccessTokenPublisherTokenDecoder {
+  override def find(
+    code: AccessTokenPublisher.AccessTokenPublisherTemporaryOauthCode
+  ): Future[Option[AccessTokenPublisher]] = {
+    val oauthURL    = "https://slack.com/api/oauth.v2.access"
+    val postedParam = Json.obj("code" -> code.value.value)
 
-    for {
+    (for {
       resp <- ws.url(oauthURL).post(postedParam) // 通信が失敗した時のハンドリング
     } yield for {
-      // 型キャストせずにcirceでレスポンス用のclassを作ってそいつにdecode / circeでjsonをdecodeできなかったらmatchでNoneで返す（エラーってこと）
-      accessToken <- AccessTokenPublisherToken((resp.json \ "access_token").as[String])
-      accessTokenPublisher = AccessTokenPublisher(accessToken, code)
-    } yield accessTokenPublisher
+      accessToken <- decode[AccessTokenPublisherToken](
+                       resp.json.toString()
+                     ).ifFailedThenReturnNone
+    } yield for {
+      accessTokenPublisher <- accessToken.map(AccessTokenPublisher(_, code))
+    } yield accessTokenPublisher).flatten
   }
 }
