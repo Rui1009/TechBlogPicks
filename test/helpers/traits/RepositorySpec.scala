7bcb3c44042936e23b5f6440730f8f228b37a1ab
Rui <3116049c@gmail.com>
2021-03-16 12:08:16 +0900

    テストの依存先の変数にlazyを追加

--- test/helpers/traits/RepositorySpec.scala
+++ test/helpers/traits/RepositorySpec.scala
@@ -3,5 +3,5 @@ package helpers.traits
 import scala.reflect.ClassTag
 
 abstract class RepositorySpec[T: ClassTag] extends UnitSpec with HasDB {
-  final val repository: T = app.injector.instanceOf[T]
+  final lazy val repository: T = app.injector.instanceOf[T]
 }
