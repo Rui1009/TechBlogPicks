7a735826f29cbf671f005a54cfd32e69098743a0
Rui <3116049c@gmail.com>
2021-03-16 12:04:51 +0900

    slackのapiからエラーが帰ってきた時、Noneを返すハンドリング追加

--- app/infra/syntax/either.scala
+++ app/infra/syntax/either.scala
@@ -1,5 +1,18 @@
 package infra.syntax
 
-class either {
+import scala.concurrent.Future
 
+object either extends InfraSyntax
+
+trait InfraSyntax {
+  implicit final def infraSyntaxEither[S, T](
+    either: Either[S, T]
+  ): EitherOps[S, T] = new EitherOps[S, T](either)
+}
+
+final private[syntax] class EitherOps[S, T](private val either: Either[S, T]) {
+  def ifFailedThenReturnNone: Future[Option[T]] = either match {
+    case Left(_)  => Future.successful(None)
+    case Right(v) => Future.successful(Some(v))
+  }
 }
