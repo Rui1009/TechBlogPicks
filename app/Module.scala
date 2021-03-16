233bce2df2293317788ed2f411d01b47ca374eb1
Rui <3116049c@gmail.com>
2021-03-16 12:06:02 +0900

    repo impl依存注入

--- app/Module.scala
+++ app/Module.scala
@@ -1,9 +1,15 @@
 import com.google.inject.AbstractModule
+import domains.accesstokenpublisher.AccessTokenPublisherRepository
 import domains.post.PostRepository
-import infra.repositoryimpl.PostRepositoryImpl
+import infra.repositoryimpl.{
+  AccessTokenPublisherRepositoryImpl,
+  PostRepositoryImpl
+}
 
 class Module extends AbstractModule {
   override def configure(): Unit = {
     bind(classOf[PostRepository]).to(classOf[PostRepositoryImpl])
+    bind(classOf[AccessTokenPublisherRepository])
+      .to(classOf[AccessTokenPublisherRepositoryImpl])
   }
 }
