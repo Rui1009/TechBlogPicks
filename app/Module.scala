843f2199d9d8e1876b1311a1ff38c1496f4d7259
Rui <3116049c@gmail.com>
2021-03-17 18:15:58 +0900

    依存注入

--- app/Module.scala
+++ app/Module.scala
@@ -1,5 +1,6 @@
 import com.google.inject.AbstractModule
 import domains.accesstokenpublisher.AccessTokenPublisherRepository
+import domains.bot.BotRepository
 import domains.post.PostRepository
 import infra.queryprocessorimpl.PublishPostsQueryProcessorImpl
 import infra.repositoryimpl._
@@ -8,12 +9,17 @@ import usecases.{InstallBotUseCase, InstallBotUseCaseImpl, RegisterPostUseCase, 
 
 class Module extends AbstractModule {
   override def configure(): Unit = {
+    // repo impl
     bind(classOf[PostRepository]).to(classOf[PostRepositoryImpl])
     bind(classOf[AccessTokenPublisherRepository])
       .to(classOf[AccessTokenPublisherRepositoryImpl])
-    bind(classOf[RegisterPostUseCase]).to(classOf[RegisterPostUseCaseImpl])
-//    bind(classOf[InstallBotUseCase]).to(classOf[InstallBotUseCaseImpl])
+    bind(classOf[BotRepository]).to(classOf[BotRepositoryImpl])
 
+    // use case impl
+    bind(classOf[RegisterPostUseCase]).to(classOf[RegisterPostUseCaseImpl])
+    bind(classOf[InstallBotUseCase]).to(classOf[InstallBotUseCaseImpl])
+
+    // query processor impl
     bind(classOf[PublishPostsQueryProcessor])
       .to(classOf[PublishPostsQueryProcessorImpl])
   }
