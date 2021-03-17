7bbf75a16f59b7c1ace9d882e14bf6002e7a2612
Rui <3116049c@gmail.com>
2021-03-17 21:24:34 +0900

    wip

--- app/infra/repositoryimpl/BotRepositoryImpl.scala
+++ app/infra/repositoryimpl/BotRepositoryImpl.scala
@@ -1,12 +1,34 @@
 package infra.repositoryimpl
 
 import com.google.inject.Inject
+import domains.bot.Bot.BotName
 import domains.bot.{Bot, BotRepository}
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.jdbc.PostgresProfile
+import infra.dto.Tables._
+import play.api.libs.json.Json
+import play.api.libs.ws._
+import io.circe.parser._
+import infra.syntax.all._
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
-class BotRepositoryImpl @Inject() extends BotRepository {
-  override def find(botId: Bot.BotId): Future[Bot] = ???
+class BotRepositoryImpl @Inject() (
+  protected val dbConfigProvider: DatabaseConfigProvider,
+  protected val ws: WSClient
+) (implicit val ec: ExecutionContext) extends HasDatabaseConfigProvider[PostgresProfile] with BotRepository {
+  override def find(botId: Bot.BotId): Future[Bot] = {
+    val getUserURL = "https://slack.com/api/users.info"
+
+    for {
+      resp <- ws.url(getUserURL).withHttpHeaders("token" -> botId.value.value).get // not found & 通信のハンドリング
+    } yield for {
+      botName <- decode[BotName](resp.json.toString)
+    } yield for {
+      accessToken <- db.run {
+        AccessTokens.filter(_.botId == botId.value).map(_.token)
+    }
+  }
 
   override def update(bot: Bot): Future[Unit] = ???
 
