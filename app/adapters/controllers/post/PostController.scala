299a702b358a0abb37c39613f1878daf5b44aa73
Daiki <daikidevelopment@gmail.com>
2021-03-17 14:09:11 +0900

    add publish

--- app/adapters/controllers/post/PostController.scala
+++ app/adapters/controllers/post/PostController.scala
@@ -5,15 +5,20 @@ import adapters.controllers.helpers.JsonHelper
 import adapters.controllers.syntax.FutureSyntax
 import com.google.inject.Inject
 import io.circe.generic.auto._
-import play.api.mvc.{Action, BaseController, ControllerComponents}
+import play.api.libs.ws.WSClient
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
+import query.publishposts.PublishPostsQueryProcessor
 import usecases.RegisterPostUseCase
 import usecases.RegisterPostUseCase.Params
+import io.circe.syntax._
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class PostController @Inject() (
   val controllerComponents: ControllerComponents,
-  registerPostUseCase: RegisterPostUseCase
+  registerPostUseCase: RegisterPostUseCase,
+  publishPostsQueryProcessor: PublishPostsQueryProcessor,
+  ws: WSClient
 )(implicit val ec: ExecutionContext)
     extends BaseController with PostCreateBodyMapper with FutureSyntax
     with JsonHelper {
@@ -37,4 +42,20 @@ class PostController @Inject() (
             .recoverError
       )
     }
+
+  def publish: Action[AnyContent] = Action.async {
+    (for {
+      publishPosts <- publishPostsQueryProcessor.findAll()
+    } yield for {
+      body <- PublishPostBody.fromViewModels(publishPosts)
+    } yield for {
+      _ <- ws.url("https://slack.com/api/chat.postMessage")
+             .post(body.asJson.noSpaces)
+    } yield ())
+      .map(Future.sequence(_))
+      .flatten
+      .ifFailedThenToAdapterError("error in PostController.publish")
+      .toSuccessGetResponse
+      .recoverError
+  }
 }
