80f3449f82a302f218b98f29eb7bcfb399cbd9d6
Daiki <daikidevelopment@gmail.com>
2021-03-17 18:50:09 +0900

    fix fromViewModels

--- app/adapters/controllers/post/PublishPostBody.scala
+++ app/adapters/controllers/post/PublishPostBody.scala
@@ -2,16 +2,28 @@ package adapters.controllers.post
 
 import query.publishposts.PublishPostsView
 
-case class PublishPostBody(token: String, channel: String, text: String)
+case class PublishPostBody(token: String, channel: String, text: Option[String])
 
 object PublishPostBody {
-  def fromViewModels(models: Seq[PublishPostsView]): Seq[PublishPostBody] =
+  def fromViewModels(
+    models: Seq[PublishPostsView],
+    tempMsg: String
+  ): Seq[PublishPostBody] =
     for {
       model   <- models
       channel <- model.channels
     } yield PublishPostBody(
       model.token,
       channel,
-      model.posts.foldLeft("")((acc, curr) => acc + "\n" + curr.url)
+      model.posts.foldLeft(Option(tempMsg))((acc, curr) =>
+        for {
+          accText <- acc
+        } yield curr.url match {
+          case Some(v) => accText + "\n" + v
+          case None    => accText
+        }
+      )
     )
 }
+
+case class PublishPostResponse(channel: String)
