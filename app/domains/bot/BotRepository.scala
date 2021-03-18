548dbe1370d3d724180883fd1db668d0c4a33782
Rui <3116049c@gmail.com>
2021-03-18 13:02:09 +0900

    bot repository のfindの返り値の型を修正

--- app/domains/bot/BotRepository.scala
+++ app/domains/bot/BotRepository.scala
@@ -5,6 +5,6 @@ import domains.bot.Bot.BotId
 import scala.concurrent.Future
 
 trait BotRepository {
-  def find(botId: BotId): Future[Bot]
+  def find(botId: BotId): Future[Option[Bot]]
   def update(bot: Bot): Future[Unit]
 }
