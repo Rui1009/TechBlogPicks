50f58dff4a833a41f816e68745e10fd5177b48f8
Rui <3116049c@gmail.com>
2021-03-18 13:02:35 +0900

    install bot usecase　の修正

--- app/usecases/InstallBotUseCase.scala
+++ app/usecases/InstallBotUseCase.scala
@@ -28,7 +28,7 @@ final class InstallBotUseCaseImpl @Inject() (
   override def exec(params: Params): Future[Unit] = for {
     targetBot            <- botRepository
                               .find(params.botId)
-                              .ifFailThenToUseCaseError(
+                              .ifNotExistsToUseCaseError(
                                 "error while botRepository.find in install bot use case"
                               )
     accessTokenPublisher <-
