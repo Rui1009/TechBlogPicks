57ff5eba5351f2d0b85befb05678b395f8729121
Rui <3116049c@gmail.com>
2021-03-18 15:13:08 +0900

    bot name decoderの修正

--- app/infra/format/BotNameDecoder.scala
+++ app/infra/format/BotNameDecoder.scala
@@ -7,7 +7,7 @@ import io.circe.Decoder
 trait BotNameDecoder {
   implicit val decodeBotName: Decoder[BotName] = Decoder.instance { cursor =>
     for {
-      botName <- cursor.downField("user").downField("id").as[String]
+      botName <- cursor.downField("user").downField("name").as[String]
     } yield BotName(Refined.unsafeApply(botName))
   }
 }
