d8579959e816262be125f7f5d5c88b69328d7c89
Daiki <daikidevelopment@gmail.com>
2021-03-22 16:51:54 +0900

    add validation

--- frontend/src/pages/PostsIndex/RegisterPostForm.tsx
+++ frontend/src/pages/PostsIndex/RegisterPostForm.tsx
@@ -1,4 +1,5 @@
-import { Formik } from "formik";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useFormik } from "formik";
 import React, { useEffect, useState } from "react";
 import {
   Button,
@@ -10,10 +11,12 @@ import {
   MenuItem,
   Paper,
   Select,
-  TextField
+  TextField,
+  Typography
 } from "@material-ui/core";
 import { format, getUnixTime, startOfToday } from "date-fns";
 import { api } from "../../utils/Api";
+import * as Yup from "yup";
 
 type Bot = { id: string; name: string };
 
@@ -32,8 +35,49 @@ const mock = [
   }
 ];
 
+type FormValues = {
+  url: string;
+  title: string;
+  author: string;
+  postedAt: string;
+  botIds: string[];
+};
+
 const RegisterPostForm: React.FC = () => {
   const [botList, setBotList] = useState<Bot[]>([]);
+
+  const formik = useFormik<FormValues>({
+    initialValues: {
+      url: "",
+      title: "",
+      author: "",
+      postedAt: format(startOfToday(), "yyyy-MM-dd"),
+      botIds: []
+    },
+    validationSchema: Yup.object({
+      url: Yup.string()
+        .min(1, "1文字以上を入力してください")
+        .required("必須です"),
+      title: Yup.string()
+        .min(1, "1文字以上を入力してください")
+        .required("必須です"),
+      author: Yup.string()
+        .min(1, "1文字以上を入力してください")
+        .required("必須です"),
+      postedAt: Yup.string().required("必須です"),
+      botIds: Yup.array().min(1, "1つ以上選択してください")
+    }),
+    onSubmit: (values, { setSubmitting }) => {
+      setTimeout(() => {
+        const req = {
+          ...values,
+          postedAt: getUnixTime(new Date(values.postedAt))
+        };
+        alert(JSON.stringify(req, null, 2));
+        setSubmitting(false);
+      }, 400);
+    }
+  });
 
   useEffect(() => {
     api.get<Bot[]>("http://localhost:9000/bots").then(v => {
@@ -44,123 +88,117 @@ const RegisterPostForm: React.FC = () => {
   }, []);
 
   return (
-    <Formik
-      initialValues={{
-        url: "",
-        title: "",
-        author: "",
-        postedAt: format(startOfToday(), "yyyy-MM-dd"),
-        botIds: []
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          const req = {
-            ...values,
-            postedAt: getUnixTime(new Date(values.postedAt))
-          };
-          alert(JSON.stringify(req, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting
-        /* and other goodies */
-      }) => (
-        <Paper style={{ width: "60%" }}>
-          <form onSubmit={handleSubmit}>
-            <Grid
-              container
-              direction={"row"}
-              alignItems={"center"}
-              style={{ padding: "12px" }}
-              justify={"space-between"}
-            >
-              <Grid item style={{ width: "100%" }}>
-                <TextField
-                  fullWidth
-                  label={"URL"}
-                  name={"url"}
-                  type={"url"}
-                  value={values.url}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item style={{ width: "100%" }}>
-                <FormControl fullWidth>
-                  <InputLabel>カテゴリー</InputLabel>
-                  <Select
-                    multiple
-                    value={values.botIds}
-                    input={<Input />}
-                    onChange={handleChange}
-                    name={"botIds"}
-                    renderValue={selected => (
-                      <Grid container direction={"row"}>
-                        {(selected as string[]).map((value, index) => (
-                          <Grid item key={value}>
-                            <Chip label={botList[index].name} />
-                          </Grid>
-                        ))}
+    <Paper style={{ width: "60%" }}>
+      <form onSubmit={formik.handleSubmit}>
+        <Grid
+          container
+          direction={"row"}
+          alignItems={"center"}
+          style={{ padding: "12px" }}
+          justify={"space-between"}
+        >
+          <Grid item style={{ width: "100%" }}>
+            <TextField
+              fullWidth
+              label={"URL"}
+              name={"url"}
+              type={"url"}
+              value={formik.values.url}
+              onChange={formik.handleChange}
+            />
+            {formik.touched.url && formik.errors.url && (
+              <Typography variant={"subtitle2"} color={"error"}>
+                {formik.errors.url}
+              </Typography>
+            )}
+          </Grid>
+          <Grid item style={{ width: "100%" }}>
+            <FormControl fullWidth>
+              <InputLabel>カテゴリー</InputLabel>
+              <Select
+                multiple
+                value={formik.values.botIds}
+                input={<Input />}
+                onChange={formik.handleChange}
+                name={"botIds"}
+                renderValue={selected => (
+                  <Grid container direction={"row"}>
+                    {(selected as string[]).map((value, index) => (
+                      <Grid item key={value}>
+                        <Chip label={botList[index].name} />
                       </Grid>
-                    )}
-                  >
-                    {botList.map(bot => (
-                      <MenuItem key={bot.id} value={bot.id}>
-                        {bot.name}
-                      </MenuItem>
                     ))}
-                  </Select>
-                </FormControl>
-              </Grid>
-              <Grid item>
-                <TextField
-                  label={"タイトル"}
-                  name={"title"}
-                  type={"text"}
-                  value={values.title}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item>
-                <TextField
-                  label={"著者"}
-                  name={"author"}
-                  type={"text"}
-                  value={values.author}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item>
-                <TextField
-                  label={"投稿日"}
-                  name={"postedAt"}
-                  type={"date"}
-                  value={values.postedAt}
-                  InputLabelProps={{ shrink: true }}
-                  onChange={handleChange}
-                />
-              </Grid>
-              <Grid item>
-                <Button
-                  type={"submit"}
-                  disabled={isSubmitting}
-                  color={"primary"}
-                >
-                  登録
-                </Button>
-              </Grid>
-            </Grid>
-          </form>
-        </Paper>
-      )}
-    </Formik>
+                  </Grid>
+                )}
+              >
+                {botList.map(bot => (
+                  <MenuItem key={bot.id} value={bot.id}>
+                    {bot.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            {formik.touched.botIds && formik.errors.botIds && (
+              <Typography variant={"subtitle2"} color={"error"}>
+                {formik.errors.botIds}
+              </Typography>
+            )}
+          </Grid>
+          <Grid item>
+            <TextField
+              label={"タイトル"}
+              name={"title"}
+              type={"text"}
+              value={formik.values.title}
+              onChange={formik.handleChange}
+            />
+            {formik.touched.title && formik.errors.title && (
+              <Typography variant={"subtitle2"} color={"error"}>
+                {formik.errors.title}
+              </Typography>
+            )}
+          </Grid>
+          <Grid item>
+            <TextField
+              label={"著者"}
+              name={"author"}
+              type={"text"}
+              value={formik.values.author}
+              onChange={formik.handleChange}
+            />
+            {formik.touched.author && formik.errors.author && (
+              <Typography variant={"subtitle2"} color={"error"}>
+                {formik.errors.author}
+              </Typography>
+            )}
+          </Grid>
+          <Grid item>
+            <TextField
+              label={"投稿日"}
+              name={"postedAt"}
+              type={"date"}
+              value={formik.values.postedAt}
+              InputLabelProps={{ shrink: true }}
+              onChange={formik.handleChange}
+            />
+            {formik.touched.postedAt && formik.errors.postedAt && (
+              <Typography variant={"subtitle2"} color={"error"}>
+                {formik.errors.postedAt}
+              </Typography>
+            )}
+          </Grid>
+          <Grid item>
+            <Button
+              type={"submit"}
+              disabled={formik.isSubmitting}
+              color={"primary"}
+            >
+              登録
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Paper>
   );
 };
 
