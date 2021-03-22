b504d823066c2e12c9b321d70ecc843e4ccbdf0a
Daiki <daikidevelopment@gmail.com>
2021-03-22 17:06:25 +0900

    remove type

--- frontend/src/utils/Api.ts
+++ frontend/src/utils/Api.ts
@@ -1,5 +1,4 @@
-import axios, { AxiosInstance } from "axios";
-import { ApiResponse } from "./types";
+import axios, { AxiosInstance, AxiosResponse } from "axios";
 
 class Api {
   constructor() {
@@ -9,11 +8,11 @@ class Api {
   private axios: AxiosInstance;
 
   get<Data>(...args: Parameters<typeof axios.get>) {
-    return this.axios.get<unknown, ApiResponse<Data>>(...args);
+    return this.axios.get<unknown, AxiosResponse<Data>>(...args);
   }
 
   post<Data>(...args: Parameters<typeof axios.post>) {
-    return this.axios.post<unknown, ApiResponse<Data>>(...args);
+    return this.axios.post<unknown, AxiosResponse<Data>>(...args);
   }
 }
 
