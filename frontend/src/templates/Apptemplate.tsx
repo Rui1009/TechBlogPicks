e3df17e12c0805b77bd8e4938a4d1577ae063a07
Rui <3116049c@gmail.com>
2021-03-22 13:20:39 +0900

    ヘッダー　＆  ドロワーの実装

--- frontend/src/templates/Apptemplate.tsx
+++ frontend/src/templates/Apptemplate.tsx
@@ -1,10 +1,46 @@
-import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Drawer,
+  Divider,
+  Typography
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import { makeStyles } from "@material-ui/core/styles";
+import { DrawerLinks } from "./components/DrawerLinks";
+
+const useStyles = makeStyles({
+  drawer: {
+    width: 240
+  }
+});
 
 export const AppTemplate: React.FC<unknown> = props => {
+  const classes = useStyles();
+
+  const [drawerOpen, setDrawerOpen] = useState(false);
   return (
-    <AppBar position={"static"}>
-      <Toolbar>aa</Toolbar>
-    </AppBar>
+    <>
+      <AppBar position={"static"}>
+        <Toolbar>
+          <IconButton edge={"start"} onClick={() => setDrawerOpen(true)}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant={"h5"}>Winkie</Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={drawerOpen} classes={{ paper: classes.drawer }}>
+        <div onClick={() => setDrawerOpen(false)}>
+          <IconButton onClick={() => setDrawerOpen(false)}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <DrawerLinks />
+      </Drawer>
+    </>
   );
 };
