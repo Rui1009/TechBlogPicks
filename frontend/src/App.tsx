d86b5808a1a860fdcd53a532c4a889b6616997c3
Rui <3116049c@gmail.com>
2021-03-22 12:26:30 +0900

    wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import { PostsIndex } from "./pages/PostsIndex/PostsIndex";
+import { AppTemplate } from "./templates/Apptemplate";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AppTemplate>
+        <Switch>
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+      </AppTemplate>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/templates/Apptemplate.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AppBar, Toolbar } from "@material-ui/core";
+
+export const AppTemplate: React.FC<unknown> = props => {
+  return (
+    <AppBar position={"static"}>
+      <Toolbar>aa</Toolbar>
+    </AppBar>
+  );
+};
