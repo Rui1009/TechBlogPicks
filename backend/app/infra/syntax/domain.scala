ddb23f294ed859a34ec3ac76dbe152719ad17a0e
Daiki <daikidevelopment@gmail.com>
2021-03-23 16:35:29 +0900

    add domain syntax for bot

--- backend/app/infra/syntax/domain.scala
+++ backend/app/infra/syntax/domain.scala
@@ -1,5 +1,6 @@
 package infra.syntax
 
+import domains.bot.Bot
 import domains.post.Post
 import infra.dto.Tables._
 
@@ -7,9 +8,10 @@ object domain extends DomainSyntax
 
 trait DomainSyntax {
   implicit final def infraSyntaxPost(model: Post): PostOps = new PostOps(model)
+  implicit final def infraSyntaxBot(model: Bot): BotOps    = new BotOps(model)
 }
 
-final private[syntax] class PostOps(private val model: Post) {
+final private[syntax] class PostOps(private val model: Post) extends AnyVal {
   def toRow(unixSec: Long): PostsRow = PostsRow(
     model.id.map(_.value.value).getOrElse(0),
     model.url.value.value,
@@ -17,5 +19,13 @@ final private[syntax] class PostOps(private val model: Post) {
     model.author.value.value,
     model.postedAt.value.value,
     unixSec
+  )
+}
+
+final private[syntax] class BotOps(private val model: Bot) extends AnyVal {
+  def toClientInfoRow: BotClientInfoRow = BotClientInfoRow(
+    model.id.value.value,
+    model.clientId.map(_.value.value),
+    model.clientSecret.map(_.value.value)
   )
 }
