2c986fc10cbfb01613f1dbb1e36395f0c14e234b
Rui <3116049c@gmail.com>
2021-03-23 12:43:19 +0900

    access token publisherの修正

--- backend/app/infra/repositoryimpl/AccessTokenPublisherRepositoryImpl.scala
+++ backend/app/infra/repositoryimpl/AccessTokenPublisherRepositoryImpl.scala
@@ -7,13 +7,13 @@ import domains.accesstokenpublisher.{
   AccessTokenPublisherRepository
 }
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
-import play.api.libs.json.Json
 import play.api.libs.ws._
 import slick.jdbc.PostgresProfile
 import slick.jdbc.PostgresProfile.API
 import infra.format.AccessTokenPublisherTokenDecoder
 import io.circe.parser._
 import infra.syntax.all._
+import io.circe.Json
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -27,12 +27,16 @@ class AccessTokenPublisherRepositoryImpl @Inject() (
   override def find(
     code: AccessTokenPublisher.AccessTokenPublisherTemporaryOauthCode
   ): Future[Option[AccessTokenPublisher]] = {
-    val oauthURL    = "https://slack.com/api/oauth.v2.access"
-    val postedParam = Json.obj("code" -> code.value.value)
+    val oauthURL = "https://slack.com/api/oauth.v2.access"
 
     for {
       resp <- ws.url(oauthURL)
-                .post(postedParam)
+                .withQueryStringParameters(
+                  "code"          -> code.value.value,
+                  "client_id"     -> "1857273131876.1857349108100",
+                  "client_secret" -> "ce17821a5d59928d7b2cd433502c6eee"
+                )
+                .post(Json.Null.noSpaces)
                 .ifFailedThenToInfraError(s"error while posting $oauthURL")
     } yield for {
       accessToken: AccessTokenPublisherToken <-
