fbe698ff053a2efe4aeca4ebc0eaf267ad1474ce
Rui <3116049c@gmail.com>
2021-03-23 16:33:42 +0900

    ドロワーにbotページへの遷移を追加

--- frontend/src/templates/components/DrawerLinks.tsx
+++ frontend/src/templates/components/DrawerLinks.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
 import AssignmentIcon from "@material-ui/icons/Assignment";
+import RedditIcon from "@material-ui/icons/Reddit";
 
 export const DrawerLinks: React.FC = () => {
   const history = useHistory();
@@ -12,6 +13,12 @@ export const DrawerLinks: React.FC = () => {
           <AssignmentIcon />
         </ListItemIcon>
         <ListItemText primary={"記事を登録する"} />
+      </ListItem>
+      <ListItem button onClick={() => history.push("/bots")}>
+        <ListItemIcon>
+          <RedditIcon />
+        </ListItemIcon>
+        <ListItemText primary={"Botを管理する"} />
       </ListItem>
     </List>
   );
