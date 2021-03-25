ec17270a8909bb4c83b02ff18b393446411a15ac
Daiki <daikidevelopment@gmail.com>
2021-03-25 15:37:51 +0900

    add endpoints constants

---
+++ frontend/src/constants/Endpoints.ts
@@ -0,0 +1,26 @@
+const SERVER_URL = process.env.SERVER_URL || "http://localhost:9000";
+
+const root = (path?: string) => {
+  return SERVER_URL + (path || "");
+};
+
+const BotsEndpoint = (path?: string) => {
+  const baseUrl = root("/bots");
+  return baseUrl + (path + "");
+};
+
+const PostEndpoint = (path?: string) => {
+  const baseUrl = root("/posts");
+  return baseUrl + (path || "");
+};
+
+PostEndpoint.publish = (path?: string) => {
+  const baseUrl = PostEndpoint("/publish");
+  return baseUrl + (path || "");
+};
+
+export const Endpoints = {
+  root,
+  bots: BotsEndpoint,
+  posts: PostEndpoint
+};
