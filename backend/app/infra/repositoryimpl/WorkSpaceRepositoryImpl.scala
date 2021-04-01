c8433ad21f4d83d1f90c0e1164b716f203eeec3e
Rui <3116049c@gmail.com>
2021-04-01 17:31:04 +0900

    redo

--- backend/app/infra/repositoryimpl/WorkSpaceRepositoryImpl.scala
+++ backend/app/infra/repositoryimpl/WorkSpaceRepositoryImpl.scala
@@ -39,9 +39,9 @@ class WorkSpaceRepositoryImpl @Inject() (
                 .withQueryStringParameters(
                   "code"          -> code.value.value,
                   "client_id"     -> clientId.value.value,
-                  "client_secret" -> clientSecret.value.value,
-                  // 実際に検証したいから一旦固定
-                  "redirect_uri"  -> "https://winkie.app"
+                  "client_secret" -> clientSecret.value.value
+//                  // 実際に検証したいから一旦固定
+//                  "redirect_uri"  -> "https://winkie.app"
                 )
                 .post(Json.Null.noSpaces)
                 .ifFailedThenToInfraError(s"error while posting $oauthURL")
