2e8482811b7ac6f910857c610ce92c13e41801e8
Rui <3116049c@gmail.com>
2021-04-13 13:10:44 +0900

    wip

--- backend/app/infra/repositoryimpl/MessageRepositoryImpl.scala
+++ backend/app/infra/repositoryimpl/MessageRepositoryImpl.scala
@@ -3,15 +3,22 @@ package infra.repositoryimpl
 import com.google.inject.Inject
 import domains.message.{Message, MessageRepository}
 import domains.workspace.WorkSpace
+import infra.dao.slack.{ConversationDao, ConversationDaoImpl}
+import play.api.libs.ws.WSClient
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class MessageRepositoryImpl @Inject() ()(implicit val ec: ExecutionContext)
+class MessageRepositoryImpl @Inject() (
+  protected val ws: WSClient,
+  protected val conversationDao: ConversationDao
+)(implicit val ec: ExecutionContext)
     extends MessageRepository {
   override def isEmpty(
     token: WorkSpace.WorkSpaceToken,
     channelId: Message.MessageChannelId
-  ): Future[Boolean] = ???
+  ): Future[Boolean] = for {
+    info <- conversationDao.info(token.value.value, channelId.value.value)
+  } yield info.isFirst()
 
   override def add(
     token: WorkSpace.WorkSpaceToken,
