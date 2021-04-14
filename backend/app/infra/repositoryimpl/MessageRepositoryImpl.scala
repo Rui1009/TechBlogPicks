f086220e64527e682e51d602c58347cc05d96703
Rui <3116049c@gmail.com>
2021-04-14 11:30:28 +0900

    message repo addを実装

--- backend/app/infra/repositoryimpl/MessageRepositoryImpl.scala
+++ backend/app/infra/repositoryimpl/MessageRepositoryImpl.scala
@@ -1,16 +1,21 @@
 package infra.repositoryimpl
 
 import com.google.inject.Inject
+import domains.message.Message.MessageBlock
 import domains.message.{Message, MessageRepository}
 import domains.workspace.WorkSpace
-import infra.dao.slack.{ConversationDao, ConversationDaoImpl}
+import infra.dao.slack.{ChatDao, ConversationDao, ConversationDaoImpl}
+import io.circe._
+import io.circe.syntax._
 import play.api.libs.ws.WSClient
+import io.circe.generic.semiauto._
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class MessageRepositoryImpl @Inject() (
   protected val ws: WSClient,
-  protected val conversationDao: ConversationDao
+  protected val conversationDao: ConversationDao,
+  protected val chatDao: ChatDao
 )(implicit val ec: ExecutionContext)
     extends MessageRepository {
   override def isEmpty(
@@ -23,6 +28,8 @@ class MessageRepositoryImpl @Inject() (
   override def add(
     token: WorkSpace.WorkSpaceToken,
     channelId: Message.MessageChannelId,
-    blocks: Seq[Message.MessageBlock]
-  ): Future[Unit] = ???
+    blocks: Seq[MessageBlock]
+  ): Future[Unit] = for {
+    _ <- chatDao.postMessage(token.value.value, channelId.value.value, blocks)
+  } yield ()
 }
