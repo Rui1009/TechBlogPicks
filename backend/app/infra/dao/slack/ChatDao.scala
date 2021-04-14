8bbfe65006fc10e27cfbf1c0b5ba7edfcd4d8862
Rui <3116049c@gmail.com>
2021-04-14 11:30:54 +0900

    blockの場合のpostMessageを実装

--- backend/app/infra/dao/slack/ChatDao.scala
+++ backend/app/infra/dao/slack/ChatDao.scala
@@ -1,14 +1,22 @@
 package infra.dao.slack
 
 import com.google.inject.Inject
+import domains.message.Message.{
+  AccessoryImage,
+  ActionBlock,
+  ActionSelect,
+  MessageBlock,
+  SectionBlock
+}
 import infra.APIError
 import infra.dao.ApiDao
 import infra.dao.slack.ChatDaoImpl.PostMessageResponse
 import infra.syntax.all._
-import io.circe.Json
+import io.circe._
 import io.circe.generic.auto._
 import io.circe.parser._
 import play.api.libs.ws.WSClient
+import io.circe.syntax._
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -17,6 +25,12 @@ trait ChatDao {
     token: String,
     channel: String,
     text: String
+  ): Future[PostMessageResponse]
+
+  def postMessage(
+    token: String,
+    channel: String,
+    blocks: Seq[MessageBlock]
   ): Future[PostMessageResponse]
 }
 
@@ -41,6 +55,57 @@ class ChatDaoImpl @Inject() (ws: WSClient)(implicit ec: ExecutionContext)
         )
       )
       .anywaySuccess(PostMessageResponse.empty)
+  }
+
+  def postMessage(
+    token: String,
+    channel: String,
+    blocks: Seq[MessageBlock]
+  ): Future[PostMessageResponse] = {
+    val url                                                = "https://slack.com/api/chat.postMessage"
+    implicit val encodeSectionBlock: Encoder[MessageBlock] = Encoder.instance {
+      case section: SectionBlock => Json.obj(
+          "type" -> Json.fromString("section"),
+          "text" -> Json.obj(
+            "type" -> Json.fromString("mrkdwn"),
+            "text" -> Json.fromString(section.blockText.text.value)
+          ),
+          section.blockAccessory match {
+            case Some(blockAccessory: AccessoryImage) =>
+              "accessory" -> Json.obj(
+                "type"      -> Json.fromString("image"),
+                "image_url" -> Json.fromString(blockAccessory.imageUrl.value),
+                "alt_text"  -> Json.fromString(blockAccessory.imageAltText)
+              )
+          }
+        )
+      case action: ActionBlock   => Json.obj(
+          "type"     -> Json.fromString("actions"),
+          "elements" -> Json.fromValues(action.actionBlockElements.map {
+            case elem: ActionSelect => Json.obj(
+                "type"        -> Json.fromString(elem.actionType.value),
+                "placeholder" -> Json.obj(
+                  "type"  -> Json.fromString("plain_text"),
+                  "text"  -> Json.fromString(elem.placeholder.placeHolderText),
+                  "emoji" -> Json.fromBoolean(elem.placeholder.placeHolderEmoji)
+                ),
+                "action_id"   -> Json.fromString(elem.actionId.value)
+              )
+          })
+        )
+    }
+
+    (for {
+      res <- ws.url(url)
+               .withHttpHeaders("Authorization" -> s"Bearer $token")
+               .withQueryStringParameters(
+                 "channel" -> channel,
+                 "blocks"  -> blocks.asJson.toString
+               )
+               .post(Json.Null.noSpaces)
+               .ifFailedThenToInfraError(s"error while posting $url")
+    } yield decode[PostMessageResponse](res.json.toString))
+      .ifLeftThenToInfraError("error while converting list api response")
   }
 }
 
