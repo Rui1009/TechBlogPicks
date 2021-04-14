25b6f98f3e22e65a1ebd7a8a78ed32e58d0056a1
Rui <3116049c@gmail.com>
2021-04-14 21:07:13 +0900

    mock を追加

--- backend/app/usecases/PostOnboardingMessageUseCase.scala
+++ backend/app/usecases/PostOnboardingMessageUseCase.scala
@@ -2,11 +2,21 @@ package usecases
 
 import com.google.inject.Inject
 import domains.bot.Bot.BotId
-import domains.message.Message.MessageChannelId
+import domains.message.Message.{
+  AccessoryImage,
+  ActionBlock,
+  ActionSelect,
+  BlockText,
+  MessageChannelId,
+  SectionBlock,
+  SelectPlaceHolder
+}
 import domains.message.MessageRepository
 import domains.workspace.WorkSpace.WorkSpaceId
 import domains.workspace.{WorkSpace, WorkSpaceRepository}
+import eu.timepit.refined.api.Refined
 import usecases.PostOnboardingMessageUseCase.Params
+import eu.timepit.refined.auto._
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -27,6 +37,8 @@ final class PostOnboardingMessageUseCaseImpl @Inject() (
   messageRepository: MessageRepository
 )(implicit val ec: ExecutionContext)
     extends PostOnboardingMessageUseCase {
+  val imageUrl                                    =
+    "https://img-mdpr.freetls.fastly.net/article/GEdz/wm/GEdzUf8cy7lGyXuQkG1UVx9LfKGYrBU76_uXSqbj86s.jpg?width=700&disable=upscale&auto=webp"
   override def exec(params: Params): Future[Unit] = (for {
     targetWorkSpace <-
       workSpaceRepository
@@ -46,7 +58,26 @@ final class PostOnboardingMessageUseCaseImpl @Inject() (
       .add(
         targetToken,
         params.channelId,
-        Seq()
+        Seq(
+          SectionBlock(
+            BlockText("test"),
+            Some(AccessoryImage(Refined.unsafeApply(imageUrl), "text"))
+          ),
+          ActionBlock(
+            Seq(
+              ActionSelect(
+                "channels_select",
+                SelectPlaceHolder("Select a channel", false),
+                "actionId-0"
+              ),
+              ActionSelect(
+                "users_select",
+                SelectPlaceHolder("Select a user", true),
+                "testId-0"
+              )
+            )
+          )
+        )
       ) // どこでblocks(投稿される内容)を定義するかは決めてないのでとりあえず空配列で
       .ifFailThenToUseCaseError(
         "error while messageRepository.add in post onboarding message use case"
