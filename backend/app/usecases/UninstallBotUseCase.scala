0861dd66b4cbe619e4303a59d49d8b2e78e0bcb4
Rui <3116049c@gmail.com>
2021-04-22 10:57:45 +0900

    uninstall use case修正

--- backend/app/usecases/UninstallBotUseCase.scala
+++ backend/app/usecases/UninstallBotUseCase.scala
@@ -1,8 +1,10 @@
 package usecases
 
 import com.google.inject.Inject
+import domains.application.Application.ApplicationId
+import domains.application.ApplicationRepository
 import domains.bot.Bot.BotId
-import domains.workspace.WorkSpace.{WorkSpaceId, WorkSpaceToken}
+import domains.workspace.WorkSpace.WorkSpaceId
 import domains.bot.BotRepository
 import domains.workspace.WorkSpaceRepository
 import usecases.UninstallBotUseCase.Params
@@ -14,31 +16,35 @@ trait UninstallBotUseCase {
 }
 
 object UninstallBotUseCase {
-  final case class Params(botId: BotId, workSpaceId: WorkSpaceId)
+  final case class Params(
+    workSpaceId: WorkSpaceId,
+    applicationId: ApplicationId
+  )
 }
 
 final class UninstallBotUseCaseImpl @Inject() (
-  botRepository: BotRepository,
-  workSpaceRepository: WorkSpaceRepository
+  workSpaceRepository: WorkSpaceRepository,
+  applicationRepository: ApplicationRepository
 )(implicit val ec: ExecutionContext)
     extends UninstallBotUseCase {
   object WorkSpaceNotFound extends Exception
 
   override def exec(params: Params): Future[Unit] = (for {
-    targetBot       <- botRepository
-                         .find(params.botId)
-                         .ifNotExistsToUseCaseError(
-                           "error while botRepository.find in uninstall bot use case"
-                         )
-    targetWorkSpace <- workSpaceRepository.find(params.workSpaceId).map {
-                         case Some(v) => v
-                         case None    => throw WorkSpaceNotFound
-                       }
-    updatedWorkSpace = targetWorkSpace.uninstallBot(targetBot)
-    _               <- workSpaceRepository
-                         .update(updatedWorkSpace)
-                         .ifFailThenToUseCaseError(
-                           "error while workSpaceRepository.add in uninstall bot use case"
-                         )
+    targetApplication <-
+      applicationRepository
+        .find(params.applicationId)
+        .ifNotExistsToUseCaseError(
+          "error while applicationRepository.find in uninstall bot use case"
+        )
+    targetWorkSpace   <- workSpaceRepository.find(params.workSpaceId).map {
+                           case Some(v) => v
+                           case None    => throw WorkSpaceNotFound
+                         }
+    updatedWorkSpace   = targetWorkSpace.uninstallApplication(targetApplication)
+    _                 <- workSpaceRepository
+                           .update(updatedWorkSpace)
+                           .ifFailThenToUseCaseError(
+                             "error while workSpaceRepository.add in uninstall bot use case"
+                           )
   } yield ()).recoverWith { case WorkSpaceNotFound => Future.unit }
 }
