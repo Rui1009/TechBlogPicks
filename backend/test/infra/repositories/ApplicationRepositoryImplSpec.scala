7ba630cc70c4881663b1b643a5402b7faf23aba4
Rui <3116049c@gmail.com>
2021-05-06 16:02:47 +0900

    application repository impl spec wip

---
+++ backend/test/infra/repositories/ApplicationRepositoryImplSpec.scala
@@ -0,0 +1,10 @@
+package infra.repositories
+
+import helpers.traits.RepositorySpec
+import infra.repositoryimpl.ApplicationRepositoryImpl
+
+class ApplicationRepositoryImplSpec
+    extends RepositorySpec[ApplicationRepositoryImpl]
+
+class ApplicationRepositoryImplSuccessSpec
+    extends ApplicationRepositoryImplSpec {}
