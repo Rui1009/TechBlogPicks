391771046dd263fd96aacee0faec631fef306f20
Rui <3116049c@gmail.com>
2021-05-10 20:05:18 +0900

    chat daoのpostMessage修正

--- backend/app/infra/dao/slack/ChatDao.scala
+++ backend/app/infra/dao/slack/ChatDao.scala
@@ -59,22 +59,40 @@ class ChatDaoImpl @Inject() (ws: WSClient)(implicit ec: ExecutionContext)
   ): Future[PostMessageResponse] = {
     val url                                                = "https://slack.com/api/chat.postMessage"
     implicit val encodeSectionBlock: Encoder[MessageBlock] = Encoder.instance {
-      case section: SectionBlock => Json.obj(
-          "type" -> Json.fromString("section"),
-          "text" -> Json.obj(
-            "type" -> Json.fromString("mrkdwn"),
-            "text" -> Json.fromString(section.blockText.text.value)
-          ),
-          section.blockAccessory match {
-            case Some(blockAccessory: AccessoryImage) =>
-              "accessory" -> Json.obj(
-                "type"      -> Json.fromString("image"),
-                "image_url" -> Json.fromString(blockAccessory.imageUrl.value),
-                "alt_text"  -> Json.fromString(blockAccessory.imageAltText)
+      case section: SectionBlock =>
+        val commonJson = JsonObject.empty
+          .add("type", Json.fromString("section"))
+          .add(
+            "text",
+            Json.fromJsonObject(
+              JsonObject.empty
+                .add("type", Json.fromString("mrkdwn"))
+                .add("text", Json.fromString(section.blockText.text.value))
+            )
+          )
+
+        section.blockAccessory match {
+          case Some(blockAccessory: AccessoryImage) => Json.fromJsonObject(
+              commonJson.add(
+                "accessory",
+                Json.fromJsonObject(
+                  JsonObject.empty
+                    .add("type", Json.fromString("image"))
+                    .add(
+                      "image_url",
+                      Json.fromString(blockAccessory.imageUrl.value)
+                    )
+                    .add(
+                      "alt_text",
+                      Json.fromString(blockAccessory.imageAltText)
+                    )
+                )
               )
-          }
-        )
-      case action: ActionBlock   => Json.obj(
+            )
+          case None                                 => Json.fromJsonObject(commonJson)
+        }
+
+      case action: ActionBlock => Json.obj(
           "type"     -> Json.fromString("actions"),
           "elements" -> Json.fromValues(action.actionBlockElements.map {
             case elem: ActionSelect => Json.obj(
@@ -95,7 +113,7 @@ class ChatDaoImpl @Inject() (ws: WSClient)(implicit ec: ExecutionContext)
                .withHttpHeaders("Authorization" -> s"Bearer $token")
                .withQueryStringParameters(
                  "channel" -> channel,
-                 "blocks"  -> blocks.asJson.toString
+                 "blocks"  -> blocks.blocks.map(_.asJson).toString
                )
                .post(Json.Null.noSpaces)
                .ifFailedThenToInfraError(s"error while posting $url")
