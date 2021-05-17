7048b0db0c119fc0dfeeece0cc09ebd30018ea56
Rui <3116049c@gmail.com>
2021-05-17 10:10:19 +0900

    デバックログ追加

--- backend/app/adapters/controllers/interactivity/InteractivityController.scala
+++ backend/app/adapters/controllers/interactivity/InteractivityController.scala
@@ -15,15 +15,20 @@ class InteractivityController @Inject() (
 )(implicit val ec: ExecutionContext)
     extends BaseController with JsonHelper with AllSyntax
     with InteractivityBodyMapper {
-  def handleInteractivity: Action[Either[AdapterError, InteractivityCommand]] =
-    Action.async(mapToInteractivityCommand) { implicit request =>
-      request.body.fold(
-        e => Future.successful(responseError(e)),
-        { case command: ChannelSelectActionInteractivityCommand =>
-          channelSelect(command)
-        }
-      )
-    }
+  def handleInteractivity = Action { implicit request =>
+    println(request.body.toString)
+    Ok("ok")
+  }
+//  def handleInteractivity: Action[Either[AdapterError, InteractivityCommand]] =
+
+//    Action.async(mapToInteractivityCommand) { implicit request =>
+//      request.body.fold(
+//        e => Future.successful(responseError(e)),
+//        { case command: ChannelSelectActionInteractivityCommand =>
+//          channelSelect(command)
+//        }
+//      )
+//    }
 
   private def channelSelect(command: ChannelSelectActionInteractivityCommand) =
     joinChannelUseCase
